c3dff5b4fd3b098bd9e23ccf9b8dbfe19d3bbab3
Rahul Rangnekar <rrangnekar96@gmail.com>
2019-01-21 23:51:19 -0800

    collect shortcode for the given profile

---
+++ index.js
@@ -0,0 +1,79 @@
+const puppeteer = require('puppeteer');
+const _ = require('lodash');
+const urlParse = require('url-parse');
+
+const args = require('yargs').options({
+  profile: {
+    alias: 'p',
+    describe: 'the profile for which you want post shortcodes',
+    demandOption: true,
+    type: 'string'
+  }
+}).argv;
+
+(async () => {
+  const { profile } = args;
+  const shortCodes = [];
+
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  page.setViewport({ height: 960, width: 1440 });
+  await page.goto(`https://instagram.com/${profile}`);
+
+  const igProfileData = await page.evaluate(() => _sharedData);
+  const user = _.get(
+    igProfileData,
+    'entry_data.ProfilePage[0].graphql.user',
+    undefined
+  );
+  if (_.isUndefined(user)) {
+    console.log(
+      `instagram profile with username "${profile}" could not be harvested. profile does not exist.`
+    );
+    await page.close();
+    await browser.close();
+  }
+  // const profileId = user.id
+
+  const firstPost = _.get(
+    user,
+    'edge_owner_to_timeline_media.edges[0].node',
+    undefined
+  );
+  if (_.isUndefined(firstPost)) {
+    console.log(
+      `instagram profile with username "${profile}" could not be harvested. no first post was found.`
+    );
+    await page.close();
+    await browser.close();
+  }
+
+  const firstPostSelector = `a[href='/p/${firstPost.shortcode}/']`;
+  await page.waitForSelector(firstPostSelector);
+  await page.click(firstPostSelector);
+
+  while (true) {
+    try {
+      await page.waitForSelector('a.coreSpriteRightPaginationArrow');
+      await page.click('a.coreSpriteRightPaginationArrow');
+      await page.waitFor(500);
+
+      const { pathname } = urlParse(page.url());
+      const shortCode = _.trimEnd(_.trimStart(pathname, '/p/'), '/');
+      shortCodes.push(shortCode);
+
+      if (shortCodes.length > 20) {
+        break;
+      }
+    } catch (e) {
+      console.log(e.message);
+      break;
+    }
+  }
+
+  console.log(shortCodes);
+
+  await page.close();
+  await browser.close();
+})();
